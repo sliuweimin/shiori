bf1ad628b1921d4af567cb272a53931a10b1f8c0
eko133 <sliuweimin@gmail.com>
2020-06-04 19:14:33 +0200

    Revert "Update root.go"
    This reverts commit 155a5b6d2facd39380d2451a9f6375d907db2e01.

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -91,7 +91,7 @@ func getDataDir(portableMode bool) (string, error) {
 
 	// Try to use platform specific app path
 	userScope := apppaths.NewScope(apppaths.User, "shiori")
-	dataDir, err := userScope.DataDirs()
+	dataDir, err := userScope.DataDir()
 	if err == nil {
 		return dataDir, nil
 	}
